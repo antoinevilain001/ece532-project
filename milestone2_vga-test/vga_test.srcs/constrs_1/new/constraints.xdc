a1170f2643eae31fb614923c4eef96eb88b82f51
Antoine Vilain <antoine.vilain@mail.utoronto.ca>
2025-02-11 00:27:07 -0500

    black-white vertical stripes test output on VGA monitor

---
+++ milestone2_vga-test/vga_test.srcs/constrs_1/new/constraints.xdc
@@ -0,0 +1,31 @@
+## Set the 100 MHz System Clock (Nexys4 DDR)
+set_property PACKAGE_PIN E3 [get_ports clk]
+set_property IOSTANDARD LVCMOS33 [get_ports clk]
+create_clock -period 10.000 [get_ports clk]
+
+## VGA Output (HSYNC, VSYNC)
+set_property PACKAGE_PIN B11 [get_ports hsync]
+set_property PACKAGE_PIN B12 [get_ports vsync]
+set_property IOSTANDARD LVCMOS33 [get_ports hsync]
+set_property IOSTANDARD LVCMOS33 [get_ports vsync]
+
+## VGA Red (4-bit)
+set_property PACKAGE_PIN A3 [get_ports {vga_r[0]}]
+set_property PACKAGE_PIN B4 [get_ports {vga_r[1]}]
+set_property PACKAGE_PIN C5 [get_ports {vga_r[2]}]
+set_property PACKAGE_PIN A4 [get_ports {vga_r[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {vga_r[*]}]
+
+## VGA Green (4-bit)
+set_property PACKAGE_PIN C6 [get_ports {vga_g[0]}]
+set_property PACKAGE_PIN A5 [get_ports {vga_g[1]}]
+set_property PACKAGE_PIN B6 [get_ports {vga_g[2]}]
+set_property PACKAGE_PIN A6 [get_ports {vga_g[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {vga_g[*]}]
+
+## VGA Blue (4-bit)
+set_property PACKAGE_PIN B7 [get_ports {vga_b[0]}]
+set_property PACKAGE_PIN C7 [get_ports {vga_b[1]}]
+set_property PACKAGE_PIN D7 [get_ports {vga_b[2]}]
+set_property PACKAGE_PIN D8 [get_ports {vga_b[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {vga_b[*]}]
---
+++ milestone2_vga-test/vga_test.srcs/sources_1/new/vga_sync.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/10/2025 11:59:04 PM
+// Design Name: 
+// Module Name: vga_sync
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vga_sync(
+    input clk,             // 25 MHz clock
+    output reg hsync,      // Horizontal sync
+    output reg vsync,      // Vertical sync
+    output reg active,     // High when pixels are in the visible area
+    output reg [9:0] x,    // X coordinate (0 - 639)
+    output reg [9:0] y     // Y coordinate (0 - 479)
+);
+    // VGA 640x480 @ 60Hz timing (25.175 MHz pixel clock)
+    localparam H_SYNC_TIME   = 96;   // Horizontal sync pulse width
+    localparam H_BACK_PORCH  = 48;   // Horizontal back porch
+    localparam H_ACTIVE_TIME = 640;  // Visible area
+    localparam H_FRONT_PORCH = 16;   // Horizontal front porch
+    localparam H_TOTAL_TIME  = H_SYNC_TIME + H_BACK_PORCH + H_ACTIVE_TIME + H_FRONT_PORCH; // Total line time (800 pixels)
+
+    localparam V_SYNC_TIME   = 2;    // Vertical sync pulse width
+    localparam V_BACK_PORCH  = 33;   // Vertical back porch
+    localparam V_ACTIVE_TIME = 480;  // Visible area
+    localparam V_FRONT_PORCH = 10;   // Vertical front porch
+    localparam V_TOTAL_TIME  = V_SYNC_TIME + V_BACK_PORCH + V_ACTIVE_TIME + V_FRONT_PORCH; // Total frame time (525 lines)
+
+    always @(posedge clk) begin
+        // Increment X coordinate
+        if (x < H_TOTAL_TIME - 1)
+            x <= x + 1;
+        else begin
+            x <= 0;
+            // Increment Y coordinate
+            if (y < V_TOTAL_TIME - 1)
+                y <= y + 1;
+            else
+                y <= 0;
+        end
+    end
+
+    // Generate sync pulses
+    always @(posedge clk) begin
+        hsync  <= (x >= H_SYNC_TIME);  // Active low sync pulse
+        vsync  <= (y >= V_SYNC_TIME);  // Active low sync pulse
+        active <= (x >= H_SYNC_TIME + H_BACK_PORCH) && (x < H_SYNC_TIME + H_BACK_PORCH + H_ACTIVE_TIME) &&
+                  (y >= V_SYNC_TIME + V_BACK_PORCH) && (y < V_SYNC_TIME + V_BACK_PORCH + V_ACTIVE_TIME);
+    end
+endmodule
+
