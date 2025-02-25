00893d0234a7ec4ed7af66e9d6681a339f11f485
Antoine Vilain <antoine.vilain@mail.utoronto.ca>
2025-02-25 13:00:15 -0500

    centering is off, appears to wrap around the screen

--- milestone3_vga-test2/top/top.srcs/sources_1/imports/ece532-project/milestone2_vga-test/vga_test.srcs/sources_1/new/vga.v
+++ milestone3_vga-test2/top/top.srcs/sources_1/imports/ece532-project/milestone2_vga-test/vga_test.srcs/sources_1/new/vga.v
@@ -42,9 +42,17 @@ module vga_bw#(
     wire [9:0] vpaddle2_x;
     wire [9:0] vpaddle2_y;
     
-    wire ball = (x > vball_x) && (x < vball_x + BALL_SIZE) && (y > vball_y) && (y < vball_y + BALL_SIZE);
-    wire border = (x < BORDER_WIDTH) || (x > GAME_WIDTH - BORDER_WIDTH) || (y < BORDER_WIDTH) || (y > GAME_HEIGHT - BORDER_WIDTH);
+    wire border_left = (x > 0) && (x < BORDER_WIDTH);
+    wire border_right =  (x > GAME_WIDTH - BORDER_WIDTH) && (x < GAME_WIDTH);
+    wire border_top = (y > 0) && (y < BORDER_WIDTH);
+    wire border_bottom = (y > GAME_HEIGHT - BORDER_WIDTH) && (y < GAME_HEIGHT);
+    wire border = border_left || border_right || border_bottom || border_top;
     wire centerlines = (x == GAME_WIDTH >> 1) || (y == GAME_HEIGHT >> 1);
+    
+    wire ball = (x > vball_x) && (x < vball_x + BALL_SIZE) && (y > vball_y) && (y < vball_y + BALL_SIZE);
+    wire paddle1 = (x > vpaddle1_x) && (x < vpaddle1_x + PADDLE_WIDTH) && (y > vpaddle1_y) && (y < vpaddle1_y + PADDLE_HEIGHT);
+    wire paddle2 = (x > vpaddle2_x) && (x < vpaddle2_x + PADDLE_WIDTH) && (y > vpaddle2_y) && (y < vpaddle2_y + PADDLE_HEIGHT);
+    
     wire final_display = border || ball || centerlines;
     
     reg video;
