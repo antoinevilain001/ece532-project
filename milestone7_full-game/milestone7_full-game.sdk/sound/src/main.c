ae373ff89de036255659d067a47c3e9d5548dcec
Antoine Vilain <antoine.vilain@mail.utoronto.ca>
2025-03-29 18:01:55 -0400

    added fsm to only play once

--- milestone7_full-game/milestone7_full-game.sdk/sound/src/main.c
+++ milestone7_full-game/milestone7_full-game.sdk/sound/src/main.c
@@ -20,6 +20,13 @@ XGpio Gpio2;
 
 XTmrCtr Timer;
 
+typedef enum {
+    IDLE,              // Waiting for switch to turn ON
+    PLAYING,           // Playing the song
+    WAIT_FOR_RELEASE   // Waiting for switch to turn OFF
+} FSM_State;
+
+
 void delay_us(u32 us) {
     XTmrCtr_Reset(&Timer, 0);
     while (XTmrCtr_GetValue(&Timer, 0) < us * (CLOCK_FREQ / 1000000));
@@ -40,6 +47,27 @@ void play_square_wave(u32 frequency_hz, u32 duration_ms) {
     }
 }
 
+void play_song1_jeopardy() {
+	play_square_wave(196, 400);   // G3
+	play_square_wave(233, 400);   // Bb3
+	play_square_wave(261, 400);   // C4
+	play_square_wave(196, 400);   // G3
+	play_square_wave(233, 400);   // Bb3
+	play_square_wave(277, 400);   // Db4
+	play_square_wave(261, 800);   // C4
+
+	delay_us(300000); // brief rest (300ms)
+
+	play_square_wave(196, 400);   // G3
+	play_square_wave(233, 400);   // Bb3
+	play_square_wave(261, 400);   // C4
+	play_square_wave(233, 400);   // Bb3
+	play_square_wave(196, 800);   // G3
+
+	delay_us(100000); // rest before repeat
+}
+
+
 int main() {
     XGpio_Initialize(&Gpio, XPAR_AXI_GPIO_0_DEVICE_ID);
     XGpio_Initialize(&Gpio1, XPAR_AXI_GPIO_1_DEVICE_ID);
@@ -47,6 +75,7 @@ int main() {
     XGpio_SetDataDirection(&Gpio2, 1, 0x0); // Outputs
     XGpio_SetDataDirection(&Gpio1, 1, 0xFFFFFFFF); // Input
     XGpio_SetDataDirection(&Gpio, PWM_GPIO_CHANNEL, ~0x3); // GPIO[0:1] as outputs
+    FSM_State state = IDLE;
 
     // Write to lights
     XGpio_DiscreteWrite(&Gpio2, 1, 0x3);
@@ -60,28 +89,31 @@ int main() {
     XGpio_DiscreteWrite(&Gpio, PWM_GPIO_CHANNEL, AMP_ENABLE_MASK);
 
     while (1) {
-    		play_square_wave(196, 400);   // G3
-    	    play_square_wave(233, 400);   // Bb3
-    	    play_square_wave(261, 400);   // C4
-    	    play_square_wave(196, 400);   // G3
-    	    play_square_wave(233, 400);   // Bb3
-    	    play_square_wave(277, 400);   // Db4
-    	    play_square_wave(261, 800);   // C4
-
-    	    delay_us(300000); // brief rest (300ms)
-
-    	    play_square_wave(196, 400);   // G3
-    	    play_square_wave(233, 400);   // Bb3
-    	    play_square_wave(261, 400);   // C4
-    	    play_square_wave(233, 400);   // Bb3
-    	    play_square_wave(196, 800);   // G3
-
-    	    delay_us(500000); // rest before repeat
-
-    	    // Read switch state (from gpio 1)
+			// Read switch state (from gpio 1)
 			u32 switch_value = XGpio_DiscreteRead(&Gpio1, 1);
 			// Write switch state to lights (to gpio 2)
 			XGpio_DiscreteWrite(&Gpio2, 1, switch_value);
+
+			// FSM Logic
+			switch (state) {
+				case IDLE:
+					if (switch_value & 0x1) {  // If switch is ON
+						state = PLAYING;
+					}
+					break;
+
+				case PLAYING:
+					play_song1_jeopardy();
+
+					state = WAIT_FOR_RELEASE;  // Move to waiting state
+					break;
+
+				case WAIT_FOR_RELEASE:
+					if (!(switch_value & 0x1)) {  // Wait until switch turns OFF
+						state = IDLE;
+					}
+					break;
+			}
     }
 
     return 0;
